8897ef156290eb4a03c6ab2f6cee3807d306985e
veenumitha <75008257+veenumitha@users.noreply.github.com>
2022-02-13 07:13:32 +1100

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ form.addEventListener ('submit', e => {
 
   });
   scrollTo(0,0);
-  result.querySelector('span').textContent = `${score}%`;
+  
   result.classList.remove("d-none");
+  let output = 0;
+const timer = setInterval (() => {
+  result.querySelector('span').textContent = `${output}%`;
+  if (output === score)
+  {
+    clearInterval(timer);
+  } else {output++;}
+},10);
+ 
 });
